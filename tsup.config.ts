f14d389d16b41e4aa1a7edbac5aefad4815deeca
WGB5445 <919603023@qq.com>
2024-11-19 02:17:29 +0800

    remove comment

--- tsup.config.ts
+++ tsup.config.ts
@@ -27,19 +27,16 @@ export function removeURLCodePlugin():Plugin {
     name: 'remove-specific-code',
     setup(build) {
       build.onLoad({ filter: /\.js$/ }, async (args) => {
-        // 读取文件内容
+
         const source = await fs.promises.readFile(args.path, 'utf8');
-        
-        // 定义要删除的代码块
+
         const regex = /if \(typeof module_or_path === 'undefined'\) {\s*module_or_path = new URL\('aptos_dynamic_transaction_composer_bg\.wasm', import\.meta\.url\);\s*}/g;
 
-        // 使用正则表达式删除代码
         const newSource = source.replace(regex, '');
 
-        // 返回删除后的代码
         return {
           contents: newSource,
-          loader: 'js', // 告诉 esbuild 这个文件是 JavaScript 文件
+          loader: 'js',
         };
       });
     },
